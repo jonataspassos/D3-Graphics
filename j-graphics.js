e4bb8c29d119af84dc540cf21f0ca36aea32bfac
jonataspassos <jonatascastropassos@gmail.com>
2020-08-28 08:26:23 -0300

    starts stack group bar chart

--- j-graphics.js
+++ j-graphics.js
@@ -804,7 +804,7 @@
 
 		return chart;
 	}
-//TODO fazer labels
+
 	jg.__chart_tree_percent_counter = 0;
 	/**
 	 * Create a tree percent chart send only the data set with the parameters key and list.
@@ -1343,4 +1343,529 @@
 		return chart;
 	}
 
+	jg.__chart_stack_group_bar_counter = 0;
+	/**
+	 * Create a stack/group-bar chart send only the data set with the parameters key and list.
+	 * Some settings can be made, like change the name of parameters of data.
+	 * @param {Array} data dataset with parameters key and list
+	 */
+	jg.chart_stack_group_bar = function (data) {
+
+		var chart = function (context) {
+
+			//General parameters
+			var transition = undefined;
+			if (context.duration)
+				transition = { duration: context.duration(), delay: context.delay(), ease: context.ease() };
+
+			var selection = context.selection ? context.selection() : context;
+			selection.classed("tree_percent-chart", true).classed("tree_percent-chart-" + chart.__id, true);
+			var size_info = jg.size_info(".tree_percent-chart-" + chart.__id)
+			chart.__size_info = size_info
+			selection.classed("tree_percent-chart-" + chart.__id, false);
+			var width = chart.width(),		//Width of Chart
+				height = chart.height(),	  //Height of chart
+				margin = chart.margin(),					  //Margin of chart
+				color = chart.color(),					   //Function to define color
+				key = chart.key(),						   //String of key
+				list = chart.list();					   //String of list
+			
+			//Tags
+			//Insert
+			selection.selectAll("svg").data([null]).enter().append("svg")
+			//Update
+			var svg = selection.select("svg").attr("width", width).attr("height", height);
+			//Insert
+			svg.selectAll("g").data([null]).enter().append("g")
+				.attr("transform", `translate(${margin.left},${margin.top})`)
+			//Update
+			var g = selection.select("g");
+
+			//Conditional Transition
+			if (transition)
+				g.transition().delay(transition.delay).duration(transition.duration)
+					.attr("transform", `translate(${margin.left},${margin.top})`);
+			else
+				g.attr("transform", `translate(${margin.left},${margin.top})`);
+
+			var h = chart.y_range()[0];
+
+			var totals = [];
+			chart.data().map((d,i)=>{
+				totals.push(d[list].reduce((total, numero) => total + numero, 0));
+			});
+
+			var x = chart.x_scale().range(chart.x_range())
+				,
+				y = chart.y_scale().range(chart.y_range()),
+				format = d3.format(".0%");
+
+			//Columns Tree
+			//Insert
+			var c_trees = g.selectAll(".c-tree").data(chart.data()).enter().append("g")
+				.attr("class", (d,i)=>"c-tree c-tree-"+i)
+				.attr("transform", function (d) { return `translate(${x(d[key])},0)` }).style("opacity",0)
+
+			c_trees.append("text").attr("class",(d,i)=>`key-label key-label-+${i}`)
+				//.attr("font-size",y.bandwidth()*0.16)
+				.attr("text-anchor","middle")
+				.attr("font-family","Roboto")
+				.attr("y",-5).attr("x",(d)=>x.bandwidth()/2)
+				.text((d,i)=>`${d.key}(${totals[i]})`).style("opacity",0);
+
+			c_trees = g.selectAll(".c-tree").selectAll(".tree").data((d,col)=>d[list].map((d,i)=>{return {d:d,i:i,col:col}})).enter().append("g")
+				.attr("class", (d,i)=>`tree tree-${i}`)
+				.attr("transform", function (d,i) { return `translate(0,${y(i)})` }).style("opacity",0)
+				
+			c_trees.selectAll("rect")
+				.data((d,i,e)=>{
+					return [
+					{d:d.d,i:d.i,e:e,x:0,y:0,w:x.bandwidth()*0.6,h:y.bandwidth()},
+					{d:d.d,i:d.i,e:e,x:x.bandwidth()*0.6,y:0,w:x.bandwidth()*0.4,h:y.bandwidth()},
+					//y:(1-d.d/totals[d.col])*y.bandwidth()	h:(d.d/totals[d.col])*y.bandwidth()
+					{d:d.d,i:d.i,e:e,col:d.col,x:x.bandwidth()*0.6,y:y.bandwidth(),w:x.bandwidth()*0.4,h:0},
+				]}).enter().append("rect").attr("class",(d,i)=>`tree-rect tree-rect-${i}`).attr("width", (d)=>d.w)
+					.attr("height", (d)=>d.h).attr("x",(d)=>d.x).attr("y",(d)=>d.y).attr("fill", color);
+			c_trees.append("text").attr("class",(d,i)=>`tree-absolute-label tree-absolute-label-${d.i}`)
+				.attr("text-anchor","middle")
+				.attr("fill","white")
+				.attr("font-weight","bold")
+				.attr("font-family","Roboto")
+				.text((d)=>d.d)
+				.attr("x",x.bandwidth()*0.6/2).attr("y",y.bandwidth()/2).attr("dy","0.8em")
+				.style("opacity",0);
+			c_trees.append("text").attr("class",(d,i)=>`tree-relative-label tree-relative-label-${d.i}`)
+				.attr("text-anchor","middle")
+				.attr("font-size",12)
+				.attr("fill","white")
+				.attr("font-family","Roboto")
+				.attr("font-weight","bold")
+				.text((d)=>`${parseInt(d.d*100/totals[d.col])}%`)
+				.attr("x",x.bandwidth()*(0.6 + 0.4/2)).attr("y",(d,i)=>y.bandwidth())
+				.attr("dy",(d,i)=>((d.d/totals[d.col])*y.bandwidth()<=14)?"-0.5em":"0.8em")
+				.style("opacity",0);
+
+			//Remove
+			c_trees = g.selectAll(".c-tree").data(chart.data()).exit();
+			trees = g.selectAll(".c-tree").data(chart.data()).selectAll(".tree").data((d)=>d[list]).exit();
+			if (transition) {
+				c_trees.style("opacity",1);
+				c_trees.transition().delay(transition.delay).duration(transition.duration * 0.3)
+					.style("opacity",0);
+				
+				c_trees.transition().delay(transition.delay + transition.duration * 0.3).remove()
+
+				trees.style("opacity",1);
+				trees.transition().delay(transition.delay).duration(transition.duration * 0.3)
+					.style("opacity",0);
+				
+				trees.transition().delay(transition.delay + transition.duration * 0.3).remove()
+			} else
+				c_trees.remove(),trees.remove()
+			
+			//Update
+			c_trees = g.selectAll(".c-tree").data(chart.data())
+			trees = c_trees.selectAll(".tree").data((d,col)=>d[list].map((e,i)=>{return {d:e,i:i,col:col,key:d[key]}}))
+				.call(chart.__event_manager.call());
+			var rects_trees = trees.selectAll("rect").data((d,i,e)=>{
+					return [
+					{d:d.d,i:d.i,e:e,x:0,y:0,w:x.bandwidth()*0.6,h:y.bandwidth()},
+					{d:d.d,i:d.i,e:e,x:x.bandwidth()*0.6,y:0,w:x.bandwidth()*0.4,h:y.bandwidth()},
+					//y:(1-d.d/totals[d.col])*y.bandwidth()	h:(d.d/totals[d.col])*y.bandwidth()
+					{d:d.d,i:d.i,e:e,col:d.col,x:x.bandwidth()*0.6,y:y.bandwidth(),w:x.bandwidth()*0.4,h:0},
+				]})
+			if (transition) {
+				// transition 1 - fade out
+				c_trees.transition().delay(transition.delay + transition.duration * 0.15).duration(transition.duration * 0.15)
+					.select(".key-label")
+					.style("opacity",0);
+				// transition 2 - position
+				c_trees.transition().delay(transition.delay + transition.duration * 0.3).duration(transition.duration * 0.4)
+					.attr("transform", function (d) { return `translate(${x(d.key)},0)` }).style("opacity",1)
+					.select(".key-label")
+					//.attr("font-size",y.bandwidth()*0.16)
+					.attr("y",-5).attr("x",(d)=>x.bandwidth()/2)
+						.text((d,i)=>`${d.key}(${totals[i]})`).style("opacity",1);
+				trees.transition().delay(transition.delay + transition.duration * 0.3).duration(transition.duration * 0.4)
+					.attr("transform", function (d) { return `translate(0,${y(d.i)})` }).style("opacity",1);
+
+				rects_trees.transition().delay(transition.delay + transition.duration * 0.3).duration(transition.duration * 0.4)
+					.attr("width", (d)=>d.w).attr("height", (d)=>d.h)
+					.attr("x",(d)=>d.x).attr("y",(d)=>d.y);
+
+				trees.select(".tree-absolute-label")
+					.transition().delay(transition.delay + transition.duration * 0.3).duration(transition.duration * 0.4)
+					.attr("x",x.bandwidth()*0.6/2).attr("y",y.bandwidth()/2)
+					.attr("dy","0.35em")
+					.attr("font-size",y.bandwidth()*0.4)
+					.tween("text", function() {
+						var i = d3.interpolate(this.textContent, 0);
+						return function(t) {
+							this.textContent = Math.round(i(t));
+						};
+					})
+					.style("opacity",0);
+				
+				trees.select(".tree-relative-label")
+					.transition().delay(transition.delay + transition.duration * 0.3).duration(transition.duration * 0.4)
+					.attr("x",x.bandwidth()*(0.6 + 0.4/2)).attr("y",(d,i)=>y.bandwidth())
+					.attr("dy","0.5em")
+					.attr("font-size",y.bandwidth()*0.16)
+					.tween("text", function(d) {
+						var i = d3.interpolate(this.textContent.match(/\d*/)[0]/100, 0);
+						return function(t) {
+							this.textContent = format(i(t));
+						};
+					})
+					
+					.style("opacity",0);
+
+				// transition 3 - data content
+				rects_trees.transition().delay(transition.delay + transition.duration * 0.7).duration(transition.duration * 0.3)
+					.attr("height", function (d,i) { return i==2?(d.d/totals[d.col])*y.bandwidth():d.h })
+					.attr("y", function (d,i) { return i==2?(1-d.d/totals[d.col])*y.bandwidth():d.y })
+					.attr("fill", color);
+
+				trees.select(".tree-absolute-label")
+					.transition().delay(transition.delay + transition.duration * 0.7).duration(transition.duration * 0.3)
+					.tween("text", function(d) {
+						var i = d3.interpolate(this.textContent, d.d);
+						return function(t) {
+							this.textContent = Math.round(i(t));
+						};
+					})
+					.style("opacity",1);
+					
+
+				
+				trees.select(".tree-relative-label")
+				.transition().delay(transition.delay + transition.duration * 0.7).duration(transition.duration * 0.3)
+					.tween("text", function(d) {
+						var i = d3.interpolate(0, d.d/totals[d.col]);
+						return function(t) {
+							this.textContent = format(i(t));
+						};
+					})
+					.attr("x",x.bandwidth()*(0.6 + 0.4/2))
+					.attr("y",(d,i)=>(1-d.d/totals[d.col]/(((d.d/totals[d.col])*y.bandwidth()<=y.bandwidth()*0.17)?1:2))*y.bandwidth())
+					.attr("dy",(d,i)=>((d.d/totals[d.col])*y.bandwidth()<=y.bandwidth()*0.17)?"-0.2em":"0.4em")
+					.style("opacity",1);
+
+			} else {
+				c_trees
+					.attr("transform", function (d) { return `translate(${x(d.key)},0)` })
+					.style("opacity",1);
+				trees
+					.attr("transform", function (d) { return `translate(0,${y(d.i)})` })
+					.style("opacity",1);
+
+				rects_trees
+					.attr("width", (d)=>d.w)
+					.attr("x",(d)=>d.x)
+					.attr("height", function (d,i) { return i==2?(d.d/totals[d.col])*y.bandwidth():d.h })
+					.attr("y", function (d,i) { return i==2?(1-d.d/totals[d.col])*y.bandwidth():d.y })
+					.attr("fill", color)
+				c_trees.select(".key-label")
+					.attr("y",-5).attr("x",(d)=>x.bandwidth()/2)
+					.text((d,i)=>`${d.key}(${totals[i]})`).style("opacity",1);
+				trees.select(".tree-absolute-label")
+					.attr("x",x.bandwidth()*0.6/2).attr("y",y.bandwidth()/2)
+					.attr("dy","0.35em")
+					.attr("font-size",y.bandwidth()*0.4)
+					.style("opacity",1);
+				trees.select(".tree-relative-label")
+					.attr("x",x.bandwidth()*(0.6 + 0.4/2))
+					.attr("y",(d,i)=>(1-d.d/totals[d.col]/(((d.d/totals[d.col])*y.bandwidth()<=y.bandwidth()*0.17)?1:2))*y.bandwidth())
+					.attr("dy",(d,i)=>((d.d/totals[d.col])*y.bandwidth()<=y.bandwidth()*0.17)?"-0.2em":"0.4em")
+					.attr("font-size",y.bandwidth()*0.16)
+					.style("opacity",1);
+
+			}
+
+		}
+
+		chart.__data = undefined
+		/**
+		 * Set data of chart as a array with parameters
+		 * specifieds in chart.key() and chart.list()
+		 * @param {Array} data list of data to set chart
+		 * @returns the self chart
+		 * If you don't send a parameter, you will get the current data of chart
+		 * If current data of chart is undefined of the data param sent is invalid,
+		 * the function will generate a random list to simulate chart
+		 */
+		chart.data = function (data) {
+			var random = false;
+
+			if (data != undefined) {
+				if (data instanceof Array) {
+					this.__data = data;
+					return this;
+				} else
+					random = true;
+			}
+			if (this.__data == undefined)
+				random = true;
+			if (random) {
+				this.key("key").list("list")
+					.__data = jg.generate(jg.getRandom(5, 10),//n elements
+						jg.getRandom(2, 3),// min value
+						jg.getRandom(4, 5))// max value
+						.map(function (d, i) {
+							d = jg.generate(d,40,350);
+							 return { key: "d" + i, list: d } })
+			}
+			return data != undefined ? this : this.__data;
+		}
+
+		chart.__width = undefined;
+		/**
+		 * Define the width of graph
+		 * @param {number} width
+		 * @returns the self chart
+		 */
+		chart.width = function (width) {
+			if (width != undefined) {
+				this.__width = width;
+				return this;
+			}
+			return this.__width || this.__size_info ? this.__size_info.w : undefined;
+		}
+		chart.__height = undefined;
+		/**
+		 * Define the height of graph
+		 * @param {number} height
+		 * @returns the self chart
+		 */
+		chart.height = function (height) {
+			if (height != undefined) {
+				this.__height = height;
+				return this;
+			}
+			return this.__height || this.__size_info ? this.__size_info.h : undefined;
+		}
+
+		chart.__margin = undefined
+		/**
+		 * Defines a margin around the chart
+		 * @param {Object} send a object with params : top, bottom, left, right
+		 * @returns the self chart
+		 * If you dont sent a parameter, you will get the current margin of chart
+		 */
+		chart.margin = function (d) {
+			if (d) {
+				var t = {}
+				t.top = d.top || this.__margin.top;
+				t.bottom = d.bottom || this.__margin.bottom;
+				t.left = d.left || this.__margin.left;
+				t.right = d.right || this.__margin.right;
+				this.__margin = t;
+				return this;
+			}
+			return chart.__margin || { top: 5, bottom: 20, left: 20, right: 5 };
+		}
+		chart.__color = undefined;
+		/**
+		 * Set a color rule to pattern of the tree to define the lines of tree.
+		 * This can be a value of color or a function with 
+		 * data(number), index(line), dataset(list of column) as param
+		 * @param {Function,String,Array} color The color rule to pattern of the tree
+		 * @param {boolean} control when true, give total control to param color (be function, String or array)
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current color rule
+		 */
+		chart.color = function (color) {
+			if (color) {
+				if(color instanceof Function)
+					this.__color = color;
+				else if(color instanceof Array)
+					this.__color = ((d,i)=>color[i]);
+				else	
+					this.__color = color;
+				return this;
+			}
+			return this.__color || ((d,i)=>(d3.schemeCategory10[i]));
+		}
+		chart.__key = undefined
+		/**
+		 * Set key index in dataset
+		 * @param {String} key Name of param in data set
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current key indexer
+		 */
+		chart.key = function (key) {
+			if (key) {
+				this.__key = key;
+				return this;
+			}
+			return this.__key || "key";
+		}
+		chart.__list = undefined
+		/**
+		 * Set list index in dataset
+		 * @param {String} list Name of param in data set
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current list indexer
+		 */
+		chart.list = function (list) {
+			if (list) {
+				this.__list = list;
+				return this;
+			}
+			return this.__list || "list";
+		}
+
+		chart.__x_scale = undefined;
+		/**
+		 * Set the x scale of the chart. this should be a scaleBand
+		 * @param {function} scaleBand
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current x scale of chart
+		 */
+		chart.x_scale = function (scaleBand) {
+			if (scaleBand) {
+				this.__x_scale = scaleBand;
+				return this;
+			} else
+				return chart.__x_scale || d3.scaleBand().domain(chart.x_domain())
+					.paddingInner(0.1)//.paddingOuter(0.1);
+		}
+
+		chart.__x_domain = undefined;
+		/**
+		 * Set domain of x scale
+		 * @param {Array} domain specify the domain
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current x domain of chart
+		 */
+		chart.x_domain = function (domain) {
+			var a = this;
+			if (domain) {
+				if (domain instanceof Array){
+					this.__x_domain = domain;
+					return this;
+				}
+			} return this.__x_domain ||
+				this.data().map(function (d) {
+					return d[a.key()]
+				})
+		}
+		chart.__x_range = undefined
+		/**
+		 * Set range of x. This should be a Array of numbers
+		 * @param {Array} range Fixed range to chart
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current x range of chart
+		 */
+		chart.x_range = function (range) {
+			if (range) {
+				this.__x_range = range;
+				return this;
+			}
+			return this.__x_range || [0, this.width() - this.margin().left - this.margin().right];
+		}
+
+		chart.__y_scale = undefined;
+		/**
+		 * Set the y scale of the chart. this should be a scaleLinear
+		 * @param {function} scaleLinear
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current y scale of chart
+		 */
+		chart.y_scale = function (scaleLinear) {
+			if (scaleLinear) {
+				this.__y_scale = scaleLinear;
+				return this;
+			} else
+				return chart.__y_scale || d3.scaleLinear().domain(chart.y_domain());
+		}
+
+		chart.__y_domain = undefined;
+		/**
+		 * Set domain of x scale
+		 * @param {Array} domain specify the domain
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current x domain of chart
+		 */
+		chart.y_domain = function (domain) {
+			var a = this
+			if (domain) {
+				if (domain instanceof Array)
+					this.__y_domain = domain;
+			} return this.__y_domain ||
+				jg.range(
+				d3.max(this.data(),
+					function (d) {
+						return d[a.list()].length
+					})
+				)
+		}
+		chart.__y_range = undefined
+		/**
+		 * Set range of y. This should be a Array of numbers
+		 * @param {Array} range Fixed range to chart
+		 * @return the self chart
+		 * If you dont sent a parameter, you will get the current y range of chart
+		 */
+		chart.y_range = function (range) {
+			if (range) {
+				this.__y_range = range;
+				return this;
+			}
+			return this.__y_range || [0, this.height() - this.margin().top - this.margin().bottom];
+		}
+
+		chart.__event_manager = new jg.EventManager();
+		/**
+		 * Set a event function
+		 * This function can recive allow parrameters:
+		 * data, index, list, tag
+		 * @param {String} type event type (click,mouseover,mouseout,...)
+		 * @param {function} func functions to execute
+		 * @param {String} name event name(use to retrive and drop the event)
+		 */
+		chart.on = function (type, func, name) {
+			this.__event_manager.event(func, type, name);
+			return this;
+		}
+		/**
+		 * Drop a event using the event name
+		 * @param {String} name event name
+		 */
+		chart.drop_event = function (name) {
+			this.__event_manager.drop(name);
+			return this;
+		}
+
+		chart.__tooltip = jg.tooltip();
+		chart.__tooltip_text = undefined;
+		/**
+		 * Set text function to activate tooltip
+		 * @param {String, Function} text
+		 * 
+		 */
+		chart.tooltip  = function(text){
+			if(text){
+				this.__tooltip.orientation("bottom-left");
+				this.on("mouseover",(d,i,e,t)=>{
+					var p = t.getBoundingClientRect();
+					chart.__tooltip.html(text instanceof Function? text(d,i,e,t): text)
+						.show(p.x+p.width,p.y)
+					d3.select(t).style("opacity",1.0).transition().style("opacity",0.7)
+				},"tooltip-in")
+				.on("mouseout",(d,i,e,t)=>{
+					chart.__tooltip.hide();
+					d3.select(t).transition().style("opacity",1)
+				},"tooltip-out")
+
+				return this;
+			}
+			return this.__tooltip;
+		}
+
+		chart.data(data);
+		chart.__id = jg.__chart_stack_group_bar_counter++;
+
+		return chart;
+	}
 })()
